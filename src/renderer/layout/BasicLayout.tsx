e1c6009fbd434d52443cf0ce543edac1a1ac4f1a
ray0324 <ray0324@foxmail.com>
2019-12-05 17:07:37 +0800

    update

--- src/renderer/layout/BasicLayout.tsx
+++ src/renderer/layout/BasicLayout.tsx
@@ -3,6 +3,10 @@ import '@/styles/global.less';
 import './BasicLayout.less';
 
 export function BasicLayout(props: any) {
+  // console.log(props.window);
+
+  const win = props.window;
+  const cls = win.isMaximized() ? 'unmaximize' : 'maximize';
   return (
     <>
       <div className="layout-title-bar">
@@ -10,13 +14,31 @@ export function BasicLayout(props: any) {
         <div className="menu"></div>
         <div className="window-title">Hello electron</div>
         <div className="window-control">
-          <i className="window-ico window-minimize"></i>
-          <i className="window-ico window-max-restore"></i>
-          <i className="window-ico window-close"></i>
+          <i className="window-ico minimize" onClick={() => win.minimize()}></i>
+          <i
+            className={`window-ico ${cls}`}
+            onClick={() =>
+              win.isMaximized() ? win.unmaximize() : win.maximize()
+            }
+          ></i>
+          <i className="window-ico close" onClick={() => win.close()}></i>
         </div>
       </div>
       <div className="layout-container">
-        <div className="layout-sidebar"></div>
+        <div className="layout-sidebar">
+          <div className="group">
+            <span className="icon iconfont icon-home"></span>
+            <span className="icon iconfont icon-layer"></span>
+            <span className="icon iconfont icon-server"></span>
+            <span className="icon iconfont icon-database"></span>
+            <span className="icon iconfont icon-secure"></span>
+            <span className="icon iconfont icon-net"></span>
+            <span className="icon iconfont icon-cpu"></span>
+          </div>
+          <div className="group">
+            <span className="icon iconfont icon-settings"></span>
+          </div>
+        </div>
         <div className="layout-main">{props.children}</div>
       </div>
     </>
--- src/renderer/pages/Main/index.tsx
+++ src/renderer/pages/Main/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import '@/assets/font/iconfont.css';
+
 import { Main } from "./Main";
 
 ReactDOM.render(<Main />, document.getElementById("root"));
